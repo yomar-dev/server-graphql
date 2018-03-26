f665bef8e1bcfedece9c6c0e4968cf41fd59fffa
Yomar Miranda <mirandayomar@gmail.com>
2018-03-25 19:50:22 -0500

    Creamos una pequeña App.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const bodyParse = require('body-parser')
+
+/**
+ * Servidor express
+ */
+const app = express()
+
+/**
+ * Puerto
+ */
+const PORT = 5678
+app.listen(PORT, () => {
+	console.log('Servidor corriendo!!')
+})
