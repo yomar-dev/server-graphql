5fcfb72a3b69455d0c243761557d6a70fb2536a6
Yomar Miranda <mirandayomar@gmail.com>
2018-03-26 22:35:45 -0500

    Creamos el resto de las entidades de nuestro schema.

--- schema.js
+++ schema.js
@@ -8,6 +8,29 @@ const typeDefs = `
 	type Curso {
 		id: ID!
 		titulo: String!
+		descripcion: String!
+		profesor: Profesor
+		rating: Float
+		comentarios: [Comentario]
+	}
+
+	type Profesor {
+		id: ID!
+		nombre: String!
+		nacionalidad: String!
+		genero: Genero
+		cursos: [Curso]
+	}
+
+	enum Genero {
+		MASCULINO
+		FEMENINO
+	}
+
+	type Comentario {
+		id: ID!
+		nombre: String!
+		cuerpo: String!
 	}
 
 	type Query {
